35e16d0c53ad598ca10a48fefb6e4c21555ff66e
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-09-18 17:59:30 +0300

    java project done!

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(shouldWakeUp(true, -1));
         System.out.println(hasTeen(22, 23, 34));
+        System.out.println(hasTeen(22, 13, 34));
         System.out.println(isCatPlaying(false, 35));
         Scanner scanner = new Scanner(System.in);
         try{
